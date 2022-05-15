00ebf1959d404f63302d3699d831c18b23eda7f3
Yudai Jinno <nakou36@gmail.com>
2022-05-16 01:26:58 +0900

    First Commit

---
+++ src/auth/Routes.tsx
@@ -0,0 +1,70 @@
+import { Routes as Switch, Route } from "react-router-dom";
+import { RequireAuth } from "./RequireAuth";
+import Login from "../pages/Login";
+import WorkloadList from "../pages/WorkloadList";
+import WorkloadRegist from "../pages/WorkloadRegist";
+import CategoryList from "../pages/CategoryList";
+import CategoryRegist from "../pages/CategoryRegist";
+import WorkTypeList from "../pages/WorktypeList";
+import WorktypeRegist from "../pages/WorktypeRegist";
+import { useAuth } from "./AuthProvider";
+import { getCategories } from "../firebase/getFirestore";
+import { useQueryCategories } from "../hooks/useQueryCategories";
+import { CircularProgress } from "@mui/material";
+
+export function Routes() {
+  const { user } = useAuth();
+  return (
+    <Switch>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <WorkloadList />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/regist"
+        element={
+          <RequireAuth>
+            <WorkloadRegist />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/category"
+        element={
+          <RequireAuth>
+            <CategoryList />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/category-regist"
+        element={
+          <RequireAuth>
+            <CategoryRegist />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/worktype"
+        element={
+          <RequireAuth>
+            <WorkTypeList />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/worktype-regist"
+        element={
+          <RequireAuth>
+            <WorktypeRegist />
+          </RequireAuth>
+        }
+      />
+    </Switch>
+  );
+}
